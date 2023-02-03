b32cd5b24d7124f0cf6bc113f0be1c729cceb8e6
Eduardo Diaz <me@iamedu.email>
2023-02-02 19:58:48 -0800

    Upgrade clojure version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.pedestal/pedestal.service "0.5.11-beta-1"]
 
                  [io.pedestal/pedestal.jetty "0.5.11-beta-1"]
